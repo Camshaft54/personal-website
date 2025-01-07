167cc5eb17efe762644fd7c702ee45986f1f939a
Camshaft54 <39676304+Camshaft54@users.noreply.github.com>
2025-01-07 01:05:49 -0800

    Set up chakra-ui and basic page structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import {Flex, Text} from "@chakra-ui/react";
+
+
+export const Home = () => {
+    return (
+        <Flex flexDirection="column">
+            <Text>Home</Text>
+        </Flex>
+    )
+}
---
+++ src/pages/Projects/index.tsx
@@ -0,0 +1,9 @@
+import {Flex, Text} from "@chakra-ui/react";
+
+export const Projects = () => {
+    return (
+        <Flex flexDirection="column">
+            <Text>Projects</Text>
+        </Flex>
+    )
+}
