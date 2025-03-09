ed29d5274eed270e22c88a2abb56901404946631
Cameron Shaw <39676304+Camshaft54@users.noreply.github.com>
2025-03-09 03:12:16 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ export const Footer = () => {
         <Flex flexDirection="column" h={24} alignItems="center" justifyContent="space-evenly"
               background={useColorModeValue("gray.100", "gray.900")}>
             <SocialIcons size={6}></SocialIcons>
-            <Text fontSize={"sm"}><Link variant="underline" href={"https://google.com"}>Open source</Link> and available
-                under the <Link variant="underline" href={"https://google.com"}>GPLv3
+            <Text fontSize={"sm"}><Link variant="underline" href={"https://github.com/Camshaft54/personal-website"}>Open source</Link> and available
+                under the <Link variant="underline" href={"https://www.gnu.org/licenses/gpl-3.0.en.html"}>GPLv3
                     License</Link>.</Text>
         </Flex>
     )
