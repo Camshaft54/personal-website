8af00b3e5b325606f891c3c073774d402679fdb6
Camshaft54 <39676304+Camshaft54@users.noreply.github.com>
2025-03-08 23:51:06 -0800

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-  },
-  plugins: [react(), tsconfigPaths()],
+    publicDir: 'public',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: [
+                        'react',
+                        'react-dom',
+                        '@chakra-ui/react',
+                        'next-themes',
+                        'framer-motion',
+                        'react-router-dom',
+                        'react-icons'
+                    ]
+                }
+            }
+        }
+    },
+    plugins: [react(), tsconfigPaths()],
 })
